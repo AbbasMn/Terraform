cd3b4874c138c4f32356b8b6d229c76bd9c2926d
sabasmn <s.abas.montaseri@gmail.com>
2022-12-23 14:25:39 -0600

    Declare custom route table

--- Terraform.tf
+++ Terraform.tf
@@ -30,3 +30,31 @@ resource "aws_internet_gateway" "internetGatway-vpc-custom" {
     Stack = "production"      
   }
 }
+
+resource "aws_route_table" "routeTable-vpc-custom" {
+  vpc_id = aws_vpc.vpc-custom.id
+  route  {
+    # carrier_gateway_id = "value"
+      cidr_block = "0.0.0.0/0"   # defualt route => all traffic eill be sent to internet gateway  
+      # core_network_arn = "value"
+      # destination_prefix_list_id = "value"
+      # egress_only_gateway_id = "value"
+      gateway_id = aws_internet_gateway.internetGatway-vpc-custom.id
+      # instance_id = "value"
+      # ipv6_cidr_block = "value"
+      # local_gateway_id = "value"
+      # nat_gateway_id = "value"
+      # network_interface_id = "value"
+      # transit_gateway_id = "value"
+      # vpc_endpoint_id = "value"
+      # vpc_peering_connection_id = "value"
+    } 
+  route {
+    ipv6_cidr_block = "::/0" # IPv6 defualt route
+    gateway_id = aws_internet_gateway.internetGatway-vpc-custom.id        
+  } 
+  tags = {
+    Name = "routeTable-vpc-custom"
+    Stack = "production"      
+  }
+}
