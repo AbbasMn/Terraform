bb82ddd69d84fc89c66bcf5619a5469679dcabca
sabasmn <s.abas.montaseri@gmail.com>
2022-12-23 15:04:09 -0600

    Declare a private subnet in custom VPC

--- Terraform.tf
+++ Terraform.tf
@@ -59,11 +59,11 @@ resource "aws_route_table" "routeTable-vpc-custom" {
   }
 }
 
-  resource "aws_subnet" "subnet-private-vpc-custom" {
-    vpc_id = aws_vpc.vpc-custom.id
-    cidr_block = "10.0.0.0/24" 
-    tags = {
-      Name = "subnet-private-vpc-custom"
-      Stack="production"
-    }
+resource "aws_subnet" "subnet-private-vpc-custom" {
+  vpc_id = aws_vpc.vpc-custom.id
+  cidr_block = "10.0.0.0/24" 
+  tags = {
+    Name = "subnet-private-vpc-custom"
+    Stack="production"
   }
+}
