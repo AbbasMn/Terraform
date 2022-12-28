816c8b0cc96b9b07c1c3a020614efd3c4a0212a6
sabasmn <s.abas.montaseri@gmail.com>
2022-12-28 17:59:26 -0600

    Eleminate AWS credentials again

--- Terraform.tf
+++ Terraform.tf
@@ -29,8 +29,8 @@ variable "subnets_cidr" {  # will load from terraform.tfvars file
 # Provision AWS region & API programming credentilas (Access key)
 provider "aws" {
   region     = "us-east-1"
-  access_key = "accessKey"
-  secret_key = "secretkey"
+  access_key = "accessKey1"
+  secret_key = "secretkey1"
 }
 
 # Provision a custom VPC
